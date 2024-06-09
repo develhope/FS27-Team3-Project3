f3d21774cbecac21f2b3fa36bb96e77b3e5f2f2c
Astrid <recoltyy@gmail.com>
2024-06-09 12:49:34 +0200

    sistemato searchbutton html e js

--- Main-js/searchbutton.js
+++ Main-js/searchbutton.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   const searchButton = document.getElementById("searchButton");
   const closeButton = document.getElementById("closeButton");
+  const searchForm = document.getElementById("searchForm")
   const searchInput = document.getElementById("searchInput");
 
   searchButton.addEventListener("click", function() {
     searchButton.style.display = "none";
     searchInput.style.display = "block";
     closeButton.style.display = "flex";
+    searchForm.style.backgroundColor = "white";
     searchInput.focus();
   });
 
@@ -14,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
     searchButton.style.display = "flex";
     searchInput.style.display = "none";
     closeButton.style.display = "none";
+    searchForm.style.backgroundColor = "transparent";
     searchInput.value = "";
   });
 });
